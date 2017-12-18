42269c61fe1b07fd4b54e85a853807b20d7e97b3
akaryanreynolds <ryanreynolds.ca@gmail.com>
2017-12-17 19:57:16 -0800

    Updated HW
    Portola session

--- word.js
+++ word.js
@@ -1,5 +1,11 @@
 
-exports.word = {
+var WordBank = ['kawasaki', 'ducati', 'honda', 'yamaha', 'suzuki', 'ktm', 'harley', 'triumph', 'indian', 'husqvarna', 'zero'];
 
-    wordBank: ['kawasaki', 'ducati', 'honda', 'yamaha', 'suzuki', 'ktm', 'harley', 'triumph', 'indian', 'husqvarna', 'zero'];
-    };
+var RandomWords = function(){
+    var index = Math.floor(Math.random() * WordBank.length);
+    this.word = WordBank[index];
+    this.lettersLength = WordBank[index].length;
+    //console.log(this.word)
+}
+
+module.exports = RandomWords
